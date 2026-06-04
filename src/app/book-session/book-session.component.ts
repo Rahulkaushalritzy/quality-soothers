de1c04e9faf7d07bfd9586b82742960b81d88db8
Rahul8286 <rahul@stellartms.com>
2026-06-05 02:05:53 +0530

    fixes -2

--- src/app/book-session/book-session.component.ts
+++ src/app/book-session/book-session.component.ts
@@ -12,7 +12,7 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 export class BookSessionComponent implements OnInit {
   public form: any;
   customerDetails: any;
-  public showDataDialog: BsModalRef<InfoModalComponent>;
+  // public showDataDialog: BsModalRef;
 
   constructor(private formBuilder: FormBuilder,
     private modalService: ModalService,
@@ -46,22 +46,55 @@ export class BookSessionComponent implements OnInit {
 
     /* Below code sends out email after user fills in session form */
 
-    emailjs.init('2HakWx77xe3kQxhbd')
-    emailjs.send("service_mc1te4e", "template_kcam8ay", {
-      from_name: `${this.form.value.firstname + '' + this.form.value.lastname}`,
+    emailjs.init('2HakWx77xe3kQxhbd');
+    emailjs.send('service_eg1d166', 'template_kcam8ay', {
+      from_name: `${this.form.value.firstname} ${this.form.value.lastname}`,
       email: this.form.value.email,
       mobile: this.form.value.mobile,
       message: this.form.value.message,
-      reply_to: "fefe",
-    }).then(
-      (response) => {
-        this.modalService.openModal(InfoModalComponent, { class: 'modal-lg modal-dialog-centered' }, data);
+      reply_to: this.form.value.email,
+    })
+    .then((response: any) => {
+      console.log('EmailJS response', response);
+
+      if (response?.status === 200) {
+        this.modalService.openModal(InfoModalComponent, { class: 'modal-lg modal-dialog-centered' }, {
+          modalData: '',
+          isError: false,
+          headings: {
+            message1: `${this.form.value.firstname}`,
+            message2: ', your details are submitted',
+            message3: 'We will contact you shortly!'
+          }
+        });
         this.form.reset();
-      },
-      (error) => {
-        this.modalService.openModal(InfoModalComponent, { class: 'modal-sm modal-dialog-centered' });
-      },
-    );
+      } else {
+        console.warn('EmailJS non-200 response', response);
+        const errorData = {
+          modalData: '',
+          isError: true,
+          headings: {
+            message1: 'Oops!',
+            message2: 'Your request could not be sent.',
+            message3: 'Please try again later.'
+          }
+        };
+        this.modalService.openModal(InfoModalComponent, { class: 'modal-sm modal-dialog-centered' }, errorData);
+      }
+    })
+    .catch((error: any) => {
+      console.error('EmailJS error', error);
+      const errorData = {
+        modalData: '',
+        isError: true,
+        headings: {
+          message1: 'Oops!',
+          message2: 'Something went wrong while sending your request.',
+          message3: 'Please try again in a moment.'
+        }
+      };
+      this.modalService.openModal(InfoModalComponent, { class: 'modal-sm modal-dialog-centered' }, errorData);
+    });
 
   }
 
